811e7e38003c1bcc06ab0fc92dda9b851a093c6f
kobayashi-oishi-kenko <kobayashi@oishi-kenko.com>
2022-09-29 21:31:59 +0900

    CookingRecordsScreenを起動画面に設定

--- app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
+++ app/src/main/java/com/oishikenko/android/recruitment/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.oishikenko.android.recruitment.feature.list.CookingRecordsScreen
 import com.oishikenko.android.recruitment.ui.theme.RecruitmentTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,24 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             RecruitmentTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                CookingRecordsScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    RecruitmentTheme {
-        Greeting("Android")
     }
 }
