aaa62c10451243d387cf8fad4a7b136cddbcb636
kobayashi-oishi-kenko <kobayashi@oishi-kenko.com>
2022-09-29 16:00:44 +0900

    setup theme

--- app/src/main/java/com/oishikenko/android/recruitment/ui/theme/Color.kt
+++ app/src/main/java/com/oishikenko/android/recruitment/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.oishikenko.android.recruitment.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val onionBeige = Color(0xFFDC9561)
+val apricotOrange = Color(0xFFDD6420)
+val mikanOrange = Color(0xFFFC962D)
+val blueberryBlue = Color(0xFF006284)
