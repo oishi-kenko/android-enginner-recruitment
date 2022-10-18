efc118befa66adf3f2d2ee9976a5eb00737ab194
kobayashi-oishi-kenko <kobayashi@oishi-kenko.com>
2022-10-18 13:08:52 +0900

    change layout composable

--- feature/src/main/java/com/oishikenko/android/recruitment/feature/list/CookingRecordListItem.kt
+++ feature/src/main/java/com/oishikenko/android/recruitment/feature/list/CookingRecordListItem.kt
@@ -1,14 +1,10 @@
 package com.oishikenko.android.recruitment.feature.list
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,7 +18,7 @@ import com.oishikenko.android.recruitment.data.model.CookingRecord
 fun CookingRecordListItem(
     cookingRecord: CookingRecord
 ) {
-    Box(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(
